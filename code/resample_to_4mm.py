6feaf523af9234255aee7ad423b602f75cb902a2
gkiar <gkiar07@gmail.com>
2019-05-10 14:07:40 +0200

    fixed minor bugs in resampling code

--- code/resample_to_4mm.py
+++ code/resample_to_4mm.py
@@ -3,11 +3,14 @@
 from argparse import ArgumentParser
 from glob import glob
 import os.path as op
+import os
 
 from nilearn.image import resample_img
 from nilearn import datasets
 import nibabel as nib
 import numpy as np
+
+import matplotlib.pyplot as plt
 
 
 shapes = {1: (182, 218, 182),
@@ -15,14 +18,19 @@ shapes = {1: (182, 218, 182),
           3: (62, 74, 62),
           4: (48, 56, 48)}
 
-def resample_images(images, outdir, resolution=4):
-    # Get file list
+def _get_imlist(images):
     if op.isdir(images):
         files = glob(op.join(images, "**", "*.nii*"), recursive=True)
         inpdir = True
     else:
         files = [images]
         inpdir = False
+    return files, inpdir
+
+
+def resample_images(images, outdir, resolution=4):
+    # Get file list
+    files, inpdir = _get_imlist(images)
 
     # Determine target affine transform
     if 1 <= resolution <= 4:
@@ -38,17 +46,35 @@ def resample_images(images, outdir, resolution=4):
                             target_affine=target_affine,
                             target_shape=target_shape)
 
-    for imfil in files:
-        newim = _resample_to_resolution(nib.load(imfil))
+    for idx, imfile in enumerate(files):
+        print("Resampling image {0} of {1}...".format(idx, len(files)))
+        newim = _resample_to_resolution(nib.load(imfile))
         if inpdir:
-            fout = op.join(outdir, op.relpath(imfil, images))
+            fout = op.join(outdir, op.relpath(imfile, images))
         else:
-            fout = op.join(outdir, op.basename(imfil))
-
+            fout = op.join(outdir, op.basename(imfile))
+        os.system("mkdir -p {0}".format(op.dirname(fout)))
         nib.save(newim, fout)
 
 
+def visualize_voxel_maps(images, outdir, resultion=4):
+    # Get file list
+    files, inpdir = _get_imlist(images)
+
+    # load brain mask from nilearn
+    # resample mask image to resolution
+    # convert to boundary, make it red
+    # for idx, imfile in enumerate(files):
+    #   load map
+    #   binarize
+    #   add intensity 1 to mask for each non zero loc
+    # set all remaining zeros to None
+    # TODO: check total count/rescale to image range
+    # visualize
+
+
 def main():
+    description = "Utility to resample nifti images in MNI space."
     parser = ArgumentParser(__file__, description=description)
     parser.add_argument("data", action="store",
                         help="Either single Nifti image file or directory to be"
@@ -60,14 +86,17 @@ def main():
                              "storing the resampled images. Images will be "
                              "stored in the same directory structure and name "
                              "convention as provided input(s).")
-    parser.add_argument("resolution", action="store", type=int, default=4,
+    parser.add_argument("--resolution", action="store", type=int, default=4,
+                        choices=[1, 2, 3, 4],
                         help="Resulting resolution in mm. Default is 4 mm.")
+    parser.add_argument("--visualize", action="store_true")
 
     results = parser.parse_args()
-    resample_images(results.data, results.outdir, results.resolution)
+    if not results.visualize:
+        resample_images(results.data, results.outdir, results.resolution)
+    else:
+        visualize_voxel_maps(results.data, results.outdir)
 
 
 if __name__ == "__main__":
     main()
-
-
